e5ffa9f562d0989888b756b1b3bba7ffaf993f95
Miracle <54377927+miraclehollins08@users.noreply.github.com>
2020-04-18 21:08:07 -0800

    Add files via upload

---
+++ App.js
@@ -0,0 +1,25 @@
+import React, { Component } from 'react';
+
+class App extends Component {
+
+    render(){
+        return (
+            <div>
+                <input type="checkbox" />
+                <p>Placeholder text here</p>
+
+                <input type="checkbox" />
+                <p>Placeholder text here</p>
+
+                <input type="checkbox" />
+                <p>Placeholder text here</p>
+
+                <input type="checkbox" />
+                <p>Placeholder text here</p>
+            </div>
+
+        )
+    }
+}
+
+export default App;
